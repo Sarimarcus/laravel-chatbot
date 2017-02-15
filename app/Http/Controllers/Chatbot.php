accb548b273f109ff0f59fadf10a39bf833044db
Olivier Depiesse <olivier.depiesse@gmail.com>
2017-02-15 12:46:18 +0100

    fix

--- app/Http/Controllers/Chatbot.php
+++ app/Http/Controllers/Chatbot.php
@@ -38,7 +38,7 @@ class Chatbot extends Controller
             // Show typing indicators
             $chatbotHelper->typingOn($senderId);
 
-            sleep(rand(2));
+            sleep(rand(1,2));
 
             // API.AI call
             $data = $chatbotHelper->getAnswer($message, 'apiai');
