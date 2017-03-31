15498cf68e44130031960de069262be464516889
Olivier Depiesse <olivier.depiesse@gmail.com>
2017-03-31 16:07:16 +0200

    only the braves ...

---
+++ deploy.php
@@ -0,0 +1,42 @@
+<?php
+namespace Deployer;
+require 'recipe/laravel.php';
+
+// Configuration
+
+set('ssh_type', 'native');
+set('ssh_multiplexing', true);
+
+set('repository', 'git@github.com:Sarimarcus/laravel-chatbot.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+
+add('writable_dirs', []);
+
+
+// Servers
+
+server('production', 'laravel-chatbot.dev.webedia.io')
+    ->user('olivier_depiesse')
+    ->identityFile()
+    ->set('deploy_path', '/var/www/laravel-chatbot')
+    ->pty(true);
+
+
+// Tasks
+
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo systemctl restart php-fpm.service');
+});
+after('deploy:symlink', 'php-fpm:restart');
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
